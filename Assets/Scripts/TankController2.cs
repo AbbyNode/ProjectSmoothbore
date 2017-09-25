d3d9822d3b2b89ce7a937a27ac819c4a8285e18f
mshah155 <300759250@cencol.ca>
2017-09-25 12:14:12 -0400

    Alternate movement (incomplete)

---
+++ Assets/Scripts/TankController2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController2 : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hMove = Input.GetAxis("Horizontal");
+		float vMove = Input.GetAxis("Vertical");
+
+		Debug.Log(hMove / vMove);
+
+		float direction = Mathf.Tan(hMove / vMove);
+
+//		Debug.Log(direction);
+
+		// rb.rotation = direction;
+	}
+
+}
