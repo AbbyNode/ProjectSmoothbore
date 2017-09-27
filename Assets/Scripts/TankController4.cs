d4523f8d97f28f39968fdd5e4fa0a7a223341880
deruip <aronly1997@hotmail.com>
2017-09-27 12:13:52 -0400

    Added two more test objects/scripts

---
+++ Assets/Scripts/TankController4.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController4 : MonoBehaviour {
+
+	public float maxSpeed = 1;
+	public float rotationSpeed = 1;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start() {
+		rb = this.GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+
+		float hMove = Input.GetAxis("Horizontal");
+		float vMove = Input.GetAxis("Vertical");
+
+		Vector3 move = Vector3.zero;
+		move.x = hMove;
+		move.y = vMove;
+
+		// Move
+
+		if (move != Vector3.zero) {
+			Vector3 dir = move;
+			float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) - 90;
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		}
+	}
+
+}
