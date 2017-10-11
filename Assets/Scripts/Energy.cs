6e2c39305d6697313800d5ea8463e35ac5f76e6b
mshah155 <300759250@cencol.ca>
2017-10-11 09:52:51 -0400

    Added event manager

--- Assets/Scripts/Energy.cs
+++ Assets/Scripts/Energy.cs
@@ -1,36 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;     //Oct 7 ,2017
+using UnityEngine.UI;
 
-public class Energy : MonoBehaviour
-{
+public class Energy : MonoBehaviour {
+	public float maxEnergy = 100;
+	public float currentEnergy;
 
-    //Oct 7 ,2017..........
-    public float maxEnergy = 100;
-    public float currentEnergy;
-    public float energyNormalSpeed = 1.0f;
+	public Slider energybar;
 
-    public Slider energybar;
+	void Start() {
+		currentEnergy = 0;
 
-    // Use this for initialization
-    void Start()
-    {
-        currentEnergy = 0;
+		energybar.value = calculateEnergy();
+	}
+	
+	float calculateEnergy() {
+		return currentEnergy / maxEnergy;
+	}
 
-        energybar.value = calculateEnergy();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Oct 7 ,2017..........
-        currentEnergy += energyNormalSpeed;
-        energybar.value = calculateEnergy();
-    }
-
-    float calculateEnergy()
-    {
-        return currentEnergy / maxEnergy;
-    }
+	public void addEnergy(float amt) {
+		currentEnergy += amt;
+		energybar.value = calculateEnergy();
+	}
 }
---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour {
+
+	private Dictionary<string, UnityEvent> _events;
+	
+	void Start() {
+		_events = new Dictionary<string, UnityEvent>();
+	}
+
+	public UnityEvent GetEvent(string name) {
+		UnityEvent e;
+
+		_events.TryGetValue(name, out e);
+
+		if (e == null) {
+			e = new UnityEvent();
+			_events.Add(name, e);
+		}
+
+		return e;
+	}
+}
