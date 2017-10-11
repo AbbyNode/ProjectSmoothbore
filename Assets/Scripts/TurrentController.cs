1b88bf4375a5224969e1bab19d3fa2001d0db60c
Jupiter1997 <jupiter.jv@gmail.com>
2017-10-11 10:21:36 -0400

    Working on Turret

---
+++ Assets/Scripts/TurrentController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurrentController : MonoBehaviour
+{
+
+    public GameObject tankShell;
+    public float fireDelta = 0.5f;
+    public Transform spawnPoint;
+
+
+    private float nextFire = 0.5f;
+    private GameObject newtankShell;
+    private float myTime = 0.0f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTime = myTime + Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            newtankShell = Instantiate(tankShell, transform.position, transform.rotation) as GameObject;
+
+            // create code here that animates the newProjectile
+          
+
+
+
+            nextFire = nextFire - myTime;
+            myTime = 0.0F;
+        }
+    }
+}
---
+++ Assets/Scripts/tankShellController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tankShellController : MonoBehaviour {
+    public float shellSpeed = 5.0f;
+
+    private Rigidbody2D rbody;
+	// Use this for initialization
+	void Start () {
+        rbody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rbody.velocity = this.transform.right * shellSpeed;
+	}
+}
