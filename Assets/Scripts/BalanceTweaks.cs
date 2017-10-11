ff51acc975a3cb6e1e81e5fa643e79c23bf11aaf
AbbyNode <abbynode@users.noreply.github.com>
2017-10-11 13:22:12 -0400

    Added balance tweaks script

---
+++ Assets/Scripts/BalanceTweaks.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnergyTweaks {
+	public float move = 0.1f;
+	public float breakCrate = 1;
+	public float hitPlayer = 2;
+	public float killPlayer = 10;
+}
+
+public class BalanceTweaks : MonoBehaviour {
+	public EnergyTweaks energyIncrements;
+}
