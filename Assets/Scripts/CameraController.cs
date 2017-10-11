3b1b2d356a39e93a5dfc019021980edbc972cc92
deruip <aronly1997@hotmail.com>
2017-10-11 12:09:59 -0400

    Added UI and Split Screen and CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Public variable to store a reference to the player game object
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
