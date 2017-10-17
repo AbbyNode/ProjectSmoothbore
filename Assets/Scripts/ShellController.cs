f568b539915039590cd263acb5f8dbed778e89e2
AbbyNode <abbynode@users.noreply.github.com>
2017-10-17 15:00:42 -0400

    Moved shell destroy to collision

---
+++ Assets/Scripts/ShellController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShellController : MonoBehaviour {
+	public float shellSpeed = 5.0f;
+
+	private Rigidbody2D rbody;
+
+	private EventManager em;
+	private UnityEvent hitEvent;
+
+	void Start() {
+		em = GlobalManager.GetPlayerEventManager(this.transform);
+
+		hitEvent = em.GetEvent("hitPlayer");
+
+		rbody = this.GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		rbody.velocity = this.transform.right * shellSpeed;
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll) {
+		Destroy(this);
+		hitEvent.Invoke();
+	}
+}
--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -3,43 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretController : MonoBehaviour {
-    public GameObject tankShell;
-    public float fireDelta = 0.5f;
-    public Transform spawnPoint;
-    public float destroyTime = 2.0f;
+	public GameObject tankShell;
+	public float fireDelta = 0.5f;
+	public Transform spawnPoint;
+	public float destroyTime = 2.0f;
 
-    private float nextFire = 0.5f;
-    private GameObject newtankShell;
-    private float myTime = 0.0f;
-    
+	private float nextFire = 0.5f;
+	private GameObject newtankShell;
+	private float myTime = 0.0f;
 
+	void Update() {
+		myTime = myTime + Time.deltaTime;
 
-    // Use this for initialization
-    void Start()
-    {
+		if (Input.GetButton("Fire1") && myTime > nextFire) {
+			nextFire = myTime + fireDelta;
 
-    }
+			newtankShell = Instantiate(tankShell, spawnPoint.position, this.transform.rotation);
 
-    // Update is called once per frame
-    void Update()
-    {
-        myTime = myTime + Time.deltaTime;
+			// create code here that animates the newProjectile
 
-        if (Input.GetButton("Fire1") && myTime > nextFire)
-        {
-            nextFire = myTime + fireDelta;
-            newtankShell = Instantiate(tankShell, spawnPoint.position, this.transform.rotation) as GameObject;
+			// Destroy(newtankShell, destroyTime);
 
-            // create code here that animates the newProjectile
-
-
-
-            Destroy(newtankShell, destroyTime);
-
-
-
-            nextFire = nextFire - myTime;
-            myTime = 0.0F;
-        }
-    }
+			nextFire = nextFire - myTime;
+			myTime = 0.0F;
+		}
+	}
 }
