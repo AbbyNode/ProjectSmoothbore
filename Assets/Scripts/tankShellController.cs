efc57f43565ced95eea58c8b2f2aef396177b8ad
AbbyNode <abbynode@users.noreply.github.com>
2017-10-17 14:40:45 -0400

    Changed layer orders, added turret to tank prefab
    Layer orders:
    -1 : Background
    0, 1  : Colliders (tanks, obstacles)
    2 : HUD

--- Assets/Scripts/tankShellController.cs
+++ Assets/Scripts/tankShellController.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class tankShellController : MonoBehaviour {
-    public float shellSpeed = 5.0f;
+	public float shellSpeed = 5.0f;
 
-    private Rigidbody2D rbody;
+	private Rigidbody2D rbody;
 
-    private EventManager em;
-    private UnityEvent dmg_playerEvent;
+	private EventManager em;
+	private UnityEvent dmg_playerEvent;
 	// Use this for initialization
-	void Start () {
-        rbody = this.GetComponent<Rigidbody2D>();
+	void Start() {
+		rbody = this.GetComponent<Rigidbody2D>();
 
 
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        rbody.velocity = this.transform.right * shellSpeed;
 	}
 
-   
+	// Update is called once per frame
+	void Update() {
+		rbody.velocity = this.transform.right * shellSpeed;
+	}
 }
