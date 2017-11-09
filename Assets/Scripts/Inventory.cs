d42cff5247195108591413f365253b764f9a2717
AbbyNode <abbynode@users.noreply.github.com>
2017-11-09 15:34:06 -0500

    Renamed energy slider UI

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+	private InventoryItem[] items;
+	private int selectedIndex;
+
+	void Start() {
+		items = new InventoryItem[8];
+	}
+
+	void Update() {
+
+	}
+
+	public void OpenInventory() {
+		selectedIndex = 0;
+	}
+
+	public void GenerateItem(InventoryItem item) {
+		for (int i = 0; i < items.Length; i++) {
+			if (items[i] == null) {
+				items[i] = item;
+				return;
+			}
+		}
+
+		Debug.Log("Inventory full");
+	}
+
+	public void DestroySelectedItem() {
+		if (items[selectedIndex] != null) {
+			Destroy(items[selectedIndex]);
+			items[selectedIndex] = null;
+		}
+	}
+}
