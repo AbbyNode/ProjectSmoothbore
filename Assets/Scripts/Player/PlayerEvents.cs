943af0de8f1f53b006148daef77c6d07a07aee3f
AbbyNode <AbbyNode@users.noreply.github.com>
2017-11-29 15:34:01 -0500

    Death event

--- Assets/Scripts/Player/PlayerEvents.cs
+++ Assets/Scripts/Player/PlayerEvents.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class PlayerEvents {
-	public static readonly string Move = "move";
-	public static readonly string EnergyChanged = "energyChanged";
-	public static readonly string BreakCrate = "BreakCreate";
+	public static readonly string Move = "Move";
+	public static readonly string EnergyChanged = "EnergyChanged";
+	public static readonly string BreakCrate = "BreakCrate";
 	public static readonly string HitPlayer = "HitPlayer";
 	public static readonly string KilledPlayer = "KilledPlayer";
+	public static readonly string PlayerDeath = "PlayerDeath";
 }
 
 /* ===============================
