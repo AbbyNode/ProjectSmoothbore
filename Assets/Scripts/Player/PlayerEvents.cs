399a99ca09ccd9befd6a9f0f882d6dd35fbc45e1
AbbyNode <AbbyNode@users.noreply.github.com>
2017-11-29 15:12:19 -0500

    Defined player events properly

---
+++ Assets/Scripts/Player/PlayerEvents.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerEvents {
+	public static readonly string Move = "move";
+	public static readonly string EnergyChanged = "energyChanged";
+	public static readonly string BreakCrate = "BreakCreate";
+	public static readonly string HitPlayer = "HitPlayer";
+	public static readonly string KilledPlayer = "KilledPlayer";
+}
+
+/* ===============================
+ * Known Event Names
+ * 
+ * move				Player moves
+ * energyChanged	Player's energy increases or decrease
+ * breakCrate		Player breaks a crate
+ * hitPlayer		Player hits another player
+ * killPlayer		Player hits another player, resulting in a kill
+ * =============================== */
