a3c45759f28e08cb3f90a8caf65c7518a72a8742
AbbyNode <AbbyNode@users.noreply.github.com>
2017-11-29 19:34:53 -0500

    Added GameManager prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,35 +4,31 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
 
-    public GameObject[] players;
-    private PlayerManager playerM;
+	public GameObject[] players;
+	private PlayerManager playerM;
 
-    // Use this for initialization
-    void Start()
-    {
-        players = GameObject.FindGameObjectsWithTag("Player");
-        EventManager eventM = playerM.eventManager;
+	// Use this for initialization
+	void Start() {
+		players = GameObject.FindGameObjectsWithTag("Player");
+		EventManager eventM = playerM.eventManager;
 
-        //for (int i = 0; i < players.Length; i++)
-        //{
-        //    if (players[i] == null)
-        //    {
-                eventM.GetEvent("PlayerDeath").AddListener(DeathCount);
-        //    }
-        //}
-    }
+		//for (int i = 0; i < players.Length; i++)
+		//{
+		//    if (players[i] == null)
+		//    {
+		eventM.GetEvent("PlayerDeath").AddListener(DeathCount);
+		//    }
+		//}
+	}
 
-    void DeathCount(float f)
-    {
-        players = GameObject.FindGameObjectsWithTag("Player");
+	void DeathCount(float f) {
+		players = GameObject.FindGameObjectsWithTag("Player");
 
-        Debug.Log("someone died");
-        if (players.Length == 1)
-        {
-            SceneManager.LoadScene("WinScene");
-        }
-    }
+		Debug.Log("someone died");
+		if (players.Length == 1) {
+			SceneManager.LoadScene("WinScene");
+		}
+	}
 }
