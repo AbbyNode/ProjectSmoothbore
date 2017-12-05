94b1755a02857c29c6fbb5b8a043a7510b23a4ba
Mr. Marvin Jupiter Roilo Vargas <mvargas2@my.centennialcollege.ca>
2017-12-05 13:33:39 -0500

    Implemented"sparkEffect"
    Added a animation folder with spark animation
    Added a hitfeedback script attach to shell prefab

---
+++ Assets/Scripts/Weapons/HitFeedback.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitFeedback : ShellController {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+	}
+
+    public GameObject SparkAni;
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        GameObject test = Instantiate(SparkAni, this.transform.position, this.transform.rotation) as GameObject;
+
+        Destroy(test,1);
+
+    }
+}
