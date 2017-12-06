5bb5cfa64ff952968bb6e46a7984f649c78924e9
AbbyNode <AbbyNode@users.noreply.github.com>
2017-12-06 09:50:59 -0500

    Updated cam shake to use event system

--- Assets/Scripts/Player/CameraShake.cs
+++ Assets/Scripts/Player/CameraShake.cs
@@ -2,55 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : CameraController {
-    Vector3 originalCameraPosition;
+public class CameraShake : MonoBehaviour {
+	public static float damageToShakeScale = 0.2f;
 
-    public float shakeAmt = 0;
+	public PlayerManager playerM;
 
-    public Camera mainCamera;
-    // Use this for initialization
-    void Start () {
+	private Camera cam;
+	private Vector3 originalCameraPosition;
+	private float shakeAmt;
 
-       // originalCameraPosition = new Vector3(this.transform.position.x, this.transform.position.y, -10);
- 
-    }
+	void Start() {
+		playerM.eventManager.GetEvent(PlayerEvents.WasHit).AddListener(WasHit);
 
-	
-	// Update is called once per frame
-	void Update () {    
-        originalCameraPosition = new Vector3(this.transform.position.x, this.transform.position.y, -10);
+		cam = this.GetComponent<Camera>();
+	}
 
+	void WasHit(float dmg) {
+		shakeAmt = dmg * damageToShakeScale;
+		// shakeAmt = coll.relativeVelocity.magnitude * .0035f;
+		originalCameraPosition = new Vector3(this.transform.position.x, this.transform.position.y, -10);
+		InvokeRepeating("CameraShaker", 0, .01f);
+		Invoke("StopShaking", 0.3f);
+	}
 
-    }
+	void CameraShaker() {
+		if (shakeAmt > 0) {
+			float quakeAmt = Random.value * shakeAmt * 2 - shakeAmt;
+			Vector3 pp = cam.transform.position;
+			pp.y += quakeAmt; // can also add to x and/or z
+			cam.transform.position = pp;
+		}
+	}
 
+	void StopShaking() {
+		CancelInvoke("CameraShaker");
+		cam.transform.position = originalCameraPosition;
+	}
 
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag=="Shell")
-        {
-            shakeAmt = coll.relativeVelocity.magnitude * .0035f;
-            InvokeRepeating("CameraShaker", 0, .01f);
-            Invoke("StopShaking", 0.3f);
-        }
-        
-
-    }
-
-    void CameraShaker()
-    {
-        if (shakeAmt > 0)
-        {
-            float quakeAmt = Random.value * shakeAmt * 2 - shakeAmt;
-            Vector3 pp = mainCamera.transform.position;
-            pp.y += quakeAmt; // can also add to x and/or z
-            mainCamera.transform.position = pp;
-        }
-    }
-
-    void StopShaking()
-    {
-        CancelInvoke("CameraShaker");
-        mainCamera.transform.position = originalCameraPosition;
-    }
-    //credits: http://newbquest.com/2014/06/the-art-of-screenshake-with-unity-2d-script/
+	//credits: http://newbquest.com/2014/06/the-art-of-screenshake-with-unity-2d-script/
 }
