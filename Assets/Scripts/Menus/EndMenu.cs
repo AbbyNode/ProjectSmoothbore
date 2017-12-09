47ceeb57b987685cb8145410c4dfca4b2ce06f79
AbbyNode <AbbyNode@users.noreply.github.com>
2017-12-08 22:44:37 -0500

    Added play again button

---
+++ Assets/Scripts/Menus/EndMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndMenu : MonoBehaviour {
+	public Button playAgainButton;
+
+	void Start() {
+		playAgainButton.onClick.AddListener(() => {
+			SceneManager.LoadScene(GameManager.mainMenuScene);
+		});
+	}
+}
