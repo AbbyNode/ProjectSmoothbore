75566df13d200a47fb84b20c5200ee1baf6db890
AbbyNode <AbbyNode@users.noreply.github.com>
2017-12-08 21:59:07 -0500

    Fixed shell in player prefab

--- Assets/Scripts/Inventory/ItemDefinitions.cs
+++ Assets/Scripts/Inventory/ItemDefinitions.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemDefinitions : MonoBehaviour {
-
-	// Use this for initialization
+	
 	void Start () {
 		
 
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
