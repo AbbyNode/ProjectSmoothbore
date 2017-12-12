28eec9fdfca30d62ebf135f4e200791992ba7a62
AbbyNode <AbbyNode@users.noreply.github.com>
2017-12-12 10:52:37 -0500

    Decoupled effect scripts
    Repurposed HitFeedback to InstOnHit

---
+++ Assets/Scripts/Effects/Explosion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+	public AudioClip explosionAudio;
+	public float destroyTime = 1;
+
+	void Start() {
+		Destroy(this.gameObject, destroyTime);
+	}
+
+	private void Awake() {
+		this.GetComponent<AudioSource>().PlayOneShot(explosionAudio, 1);
+	}
+}
---
+++ Assets/Scripts/Effects/InstOnHit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstOnHit : MonoBehaviour {
+	public GameObject objToInst;
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		GameObject obj = Instantiate(this.objToInst, this.transform.position, this.transform.rotation) as GameObject;
+	}
+}
