576ebf4f0e292eb7e9f95545fcd427d9c57a326b
Mr. Marvin Jupiter Roilo Vargas <mvargas2@my.centennialcollege.ca>
2017-12-17 15:27:38 -0500

    Added World SpaceUI

--- Assets/Scripts/UI/HealthSlider.cs
+++ Assets/Scripts/UI/HealthSlider.cs
@@ -6,4 +6,18 @@ public class HealthSlider : StatSlider {
 	private void Start() {
 		this.Init(PlayerEvents.HealthChanged);
 	}
+    Quaternion rotation;
+    Vector3 position;
+  
+    private void Awake()
+    {
+        rotation = transform.rotation;
+        position = transform.parent.position - transform.position;
+    }
+    private void Update()
+    {
+        this.transform.rotation = rotation;
+        this.transform.position = transform.parent.position - position;
+
+    }
 }
