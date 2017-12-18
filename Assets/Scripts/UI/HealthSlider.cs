7e60ae4f0525934904efb1b8ebff0180207de028
deruip <aronly1997@hotmail.com>
2017-12-18 12:14:15 -0500

    Added Health Bar fading

--- Assets/Scripts/UI/HealthSlider.cs
+++ Assets/Scripts/UI/HealthSlider.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HealthSlider : StatSlider {
-	private void Start() {
-		this.Init(PlayerEvents.HealthChanged);
-	}
+public class HealthSlider : StatSlider
+{
     Quaternion rotation;
     Vector3 position;
-  
+    float timeSinceHit;
+    public Image background, fill;
+
+    private void Start()
+    {
+        playerM.eventManager.GetEvent(PlayerEvents.WasHit).AddListener(ShowUI);
+        this.Init(PlayerEvents.HealthChanged);
+    }
+
     private void Awake()
     {
         rotation = transform.rotation;
@@ -18,6 +25,21 @@ public class HealthSlider : StatSlider {
     {
         this.transform.rotation = rotation;
         this.transform.position = transform.parent.position - position;
-
+        timeSinceHit += Time.deltaTime;
+        if (timeSinceHit >= 3)
+        {
+            FadeUI();
+        }
+    }
+    private void FadeUI()
+    {
+        background.CrossFadeAlpha(0, 1, false);
+        fill.CrossFadeAlpha(0, 1, false);
+    }
+    private void ShowUI(float x)
+    {
+        timeSinceHit = 0;
+        background.CrossFadeAlpha(1, 0, false);
+        fill.CrossFadeAlpha(1, 0, false);
     }
 }
