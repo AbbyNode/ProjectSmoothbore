121a5470e632c07de2c25952cdf9ceecea4c2e82
AbbyNode <AbbyNode@users.noreply.github.com>
2017-12-18 11:17:56 -0500

    Fixed crate collision

--- Assets/Scripts/InstDestroy/InstOnHit.cs
+++ Assets/Scripts/InstDestroy/InstOnHit.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InstOnHit : MonoBehaviour {
+	public string hitTag;
 	public GameObject objToInst;
 
 	void OnCollisionEnter2D(Collision2D coll) {
-		GameObject obj = Instantiate(this.objToInst, this.transform.position, this.transform.rotation) as GameObject;
+		if (coll.gameObject.CompareTag(hitTag)) {
+			GameObject obj = Instantiate(this.objToInst, this.transform.position, this.transform.rotation) as GameObject;
+		}
 	}
 }
