cae65d409a4e3586ce0341ae151936daa127eeb4
deruip <aronly1997@hotmail.com>
2018-01-03 13:21:58 -0500

    Added lives counter

--- Assets/Scripts/Player/Stats/Health.cs
+++ Assets/Scripts/Player/Stats/Health.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : PlayerStat {
-	void Start() {
+    public int lives = 3;
+    void Start() {
 		EventManager eventM = playerM.eventManager;
 		HealthTweaks tweaks = BalanceTweaks.GlobalInstance.health;
         Vector3 spawnPoint = playerM.tankObj.transform.position;
-        int lives = 3;
 		
 		Init(eventM.GetEvent(PlayerEvents.HealthChanged), tweaks.player);
 		SetStatValue(tweaks.player);
@@ -22,7 +22,7 @@ public class Health : PlayerStat {
                 SetStatValue(100);
                 lives -= 1;
 
-				wasKilledEvent.Invoke(0);
+				wasKilledEvent.Invoke(lives);
 			}
             if (lives <= 0)
             {
---
+++ Assets/Scripts/UI/LivesUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesUI : MonoBehaviour {
+	public PlayerManager playerM;
+
+	private Text livesUI;
+
+	void Start() {
+		this.livesUI = this.GetComponent<Text>();
+        livesUI.text = "Lives: " + playerM.health.lives;
+
+		EventManager eventM = playerM.eventManager;
+		eventM.GetEvent(PlayerEvents.WasKilled).AddListener((lives) => livesUI.text = "Lives: " + lives);
+        eventM.GetEvent(PlayerEvents.Lost).AddListener((lives) => livesUI.text = "Out of Lives!");
+    }
+}
