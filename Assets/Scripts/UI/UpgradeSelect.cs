ea9f66e67138ebfd49119205fa2cec2dddcf5da2
deruip <aronly1997@hotmail.com>
2018-01-03 20:10:25 -0500

    Testing module selection popup

---
+++ Assets/Scripts/UI/UpgradeSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeSelect : MonoBehaviour
+{
+    public PlayerManager playerM;
+    void Start()
+    {
+        EventManager eventM = playerM.eventManager;
+        eventM.GetEvent(PlayerEvents.EnergyChanged).AddListener((f) =>
+        {
+            Debug.Log("energychanged");
+            if (playerM.energy.GetStatValue() >= 50)
+            {
+                Debug.Log("energy over 50");
+                this.gameObject.SetActive(true);
+            }
+        });
+    }
+
+}
