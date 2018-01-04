fa646e539c3604533c884e450577aa9b1e31e6c5
AbbyNode <AbbyNode@users.noreply.github.com>
2018-01-03 21:19:37 -0500

    Updated skull sprite
    Using BalanceTweaks for module select

--- Assets/Scripts/UI/UpgradeSelect.cs
+++ Assets/Scripts/UI/UpgradeSelect.cs
@@ -2,19 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpgradeSelect : MonoBehaviour
-{
-    public PlayerManager playerM;
-    void Start()
-    {
-        EventManager eventM = playerM.eventManager;
-        eventM.GetEvent(PlayerEvents.EnergyChanged).AddListener((f) =>
-        {
-            if (playerM.energy.GetStatValue() >= 50)
-            {
-                this.GetComponent<CanvasGroup>().alpha = 1;
-            }
-        });
-    }
+public class UpgradeSelect : MonoBehaviour {
+	public PlayerManager playerM;
 
+	void Start() {
+		EventManager eventM = playerM.eventManager;
+		eventM.GetEvent(PlayerEvents.EnergyChanged).AddListener((f) => {
+			if (playerM.energy.GetStatValue() >= BalanceTweaks.GlobalInstance.moduleCosts.gunUpgrade) {
+				this.GetComponent<CanvasGroup>().alpha = 1;
+			}
+		});
+	}
 }
