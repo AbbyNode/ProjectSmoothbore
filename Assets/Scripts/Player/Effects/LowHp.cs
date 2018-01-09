4a6bcf666c8c9e583a1b4d4c89d6de0808b1d103
AbbyNode <AbbyNode@users.noreply.github.com>
2018-01-09 07:35:27 -0500

    Added low hp smoke

---
+++ Assets/Scripts/Player/Effects/LowHp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowHp : MonoBehaviour {
+	public PlayerManager playerM;
+	public float maxEmission = 6;
+
+	private ParticleSystem particles;
+
+	void Start() {
+		particles = this.GetComponent<ParticleSystem>();
+
+		playerM.eventManager.GetEvent(PlayerEvents.HealthChanged).AddListener((health) => {
+			ParticleSystem.EmissionModule em = particles.emission;
+			em.rateOverTime = ((100 - health) / 100 * maxEmission);
+		});
+	}
+}
